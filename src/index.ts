17cd4df35aa04c0dcc54250cb57449284595622e
Enzo Ferrari <ferrarienzobf@gmail.com>
2023-10-22 15:52:55 -0300

    Revert "Remove application insights"
    This reverts commit 17efce3c527f026068a8bf37dc405d6117d71726.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import 'reflect-metadata'
 
+import * as applicationInsights from 'applicationinsights'
+
+applicationInsights
+  .setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
+  .setDistributedTracingMode(applicationInsights.DistributedTracingModes.AI_AND_W3C)
+  .setSendLiveMetrics(true)
+  .start()
+
 import Container from 'typedi'
 
 import { Application } from './Application'
